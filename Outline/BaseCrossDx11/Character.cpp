85fdb6422483c195a1f465fe13c6a1f992fa26a7
Kyohei <kurokenken0310@gmail.com>
2017-10-02 21:48:21 +0900

    Create project

---
+++ Outline/BaseCrossDx11/Character.cpp
@@ -0,0 +1,114 @@
+/*!
+@file Character.cpp
+@brief キャラクターなど実体
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+	TorusObject::TorusObject(){
+
+	}
+
+	TorusObject::~TorusObject() {
+
+	}
+
+	void TorusObject::OnCreate() {
+		vector<VertexPositionNormalTexture> vertices;
+		vector<uint16_t> indices;
+		MeshUtill::CreateTorus(1, 0.5f, 100, vertices, indices);
+		m_SphereMesh = MeshResource::CreateMeshResource(vertices, indices, false);
+
+		m_Rotation.identity();
+
+		wstring assetDir;
+		App::GetApp()->GetAssetsDirectory(assetDir);
+		wstring texStr = assetDir + L"Checker.png";
+		m_TextureResource = TextureResource::CreateTextureResource(texStr);
+	}
+
+	void TorusObject::OnUpdate() {
+		Quat QtSpan;
+		QtSpan.rotation(0.02f, Vec3(1, 0, 1));
+		m_Rotation *= QtSpan;
+		m_Rotation.normalize();
+	}
+
+	void TorusObject::OnDraw() {
+		Mat4x4 World;
+		
+		World.affineTransformation(
+			Vec3(1, 1, 1),
+			Vec3(0, 0, 0),
+			m_Rotation,
+			Vec3(0, 0, 0)
+		);
+
+		auto Dev = App::GetApp()->GetDeviceResources();
+		auto pD3D11DeviceContext = Dev->GetD3DDeviceContext();
+		auto RenderState = Dev->GetRenderState();
+
+		pD3D11DeviceContext->VSSetShader(VSPNTStatic::GetPtr()->GetShader(), nullptr, 0);
+		pD3D11DeviceContext->PSSetShader(PSPNTStatic::GetPtr()->GetShader(), nullptr, 0);
+
+		pD3D11DeviceContext->IASetInputLayout(VSPNTStatic::GetPtr()->GetInputLayout());
+
+		pD3D11DeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+		pD3D11DeviceContext->OMSetDepthStencilState(RenderState->GetDepthDefault(), 0);
+
+		ID3D11SamplerState* pSamplerWrap = RenderState->GetLinearWrap();
+
+		UINT stride = sizeof(VertexPositionNormalTexture);
+		UINT offset = 0;
+
+		Mat4x4 View, Proj;
+		Vec3 CameraPos = Vec3(0, 5, -2);
+		View = XMMatrixLookAtLH(CameraPos, Vec3(0, 0, 0), Vec3(0, 1, 0));
+		float w = static_cast<float>(App::GetApp()->GetGameWidth());
+		float h = static_cast<float>(App::GetApp()->GetGameHeight());
+		Proj = XMMatrixPerspectiveFovLH(XM_PIDIV4, w / h, 1.0f, 100.0f);
+
+		Vec4 LightDir = Vec4(-1, -2, -3, 1);
+
+		View.transpose();
+		Proj.transpose();
+
+		PNStaticConstantBuffer sb;
+		sb.View = View;
+		sb.CameraLocation = CameraPos;
+		sb.Projection = Proj;
+		sb.LightDir = LightDir;
+
+		sb.Diffuse = Col4(1, 1, 1, 1.0f);
+		sb.Emissive = Col4(0.25f);
+
+		World.transpose();
+		sb.World = World;
+
+		pD3D11DeviceContext->UpdateSubresource(CBPNStatic::GetPtr()->GetBuffer(), 0, nullptr, &sb, 0, 0);
+
+		ID3D11Buffer* pConstantBuffer = CBPNStatic::GetPtr()->GetBuffer();
+
+		pD3D11DeviceContext->VSSetConstantBuffers(0, 1, &pConstantBuffer);
+		pD3D11DeviceContext->PSSetConstantBuffers(0, 1, &pConstantBuffer);
+		pD3D11DeviceContext->IASetVertexBuffers(0, 1, m_SphereMesh->GetVertexBuffer().GetAddressOf(), &stride, &offset);
+		pD3D11DeviceContext->IASetIndexBuffer(m_SphereMesh->GetIndexBuffer().Get(), DXGI_FORMAT_R16_UINT, 0);
+
+		pD3D11DeviceContext->PSSetSamplers(0, 1, &pSamplerWrap);
+
+		pD3D11DeviceContext->OMSetBlendState(RenderState->GetOpaque(), nullptr, 0xffffffff);
+		pD3D11DeviceContext->RSSetState(RenderState->GetCullBack());
+
+		//テクスチャ
+		pD3D11DeviceContext->PSSetShaderResources(0, 1, m_TextureResource->GetShaderResourceView().GetAddressOf());
+
+		pD3D11DeviceContext->DrawIndexed(m_SphereMesh->GetNumIndicis(), 0, 0);
+
+		Dev->InitializeStates();
+	}
+
+}
+//end basecross
---
+++ Outline/DxShared/Shaders/VSPNTStatic.hlsl
@@ -0,0 +1,33 @@
+#include "INCStructs.hlsli"
+
+cbuffer SimpleConstantBuffer : register(b0)
+{
+	float4x4 World : packoffset(c0);
+	float4x4 View : packoffset(c4);
+	float4x4 Projection : packoffset(c8);
+	float4 LightDir : packoffset(c12);
+	float4 Emissive : packoffset(c13);
+	float4 Diffuse : packoffset(c14);
+	float4 CameraLocation : packoffset(c15);
+}
+
+PSPNTInput main(VSPNTInput input)
+{
+	PSPNTInput result;
+
+	float4 pos = float4(input.position.xyz, 1.0f);
+	pos = mul(pos, World);
+	pos = mul(pos, View);
+	pos = mul(pos, Projection);
+
+	result.position = pos;
+
+	result.norm = mul(input.norm, (float3x3)World);
+	result.norm = normalize(result.norm);
+
+	result.tex = input.tex;
+
+	result.eyeDir = normalize(CameraLocation - input.position.xyz);
+
+	return result;
+}
